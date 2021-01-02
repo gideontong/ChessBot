2e307447b18b30cc71ccf47b7557345a0e053133
Gideon Tong <gideon@gideontong.com>
2021-01-01 17:27:12 -0800

    Add game runtime format

--- runtime.js
+++ runtime.js
@@ -17,13 +17,40 @@ module.exports = async (channel, message, args) => {
     if (player2.user.bot) {
         return channel.send('Playing against a bot makes no sense... they don\'t have the ability to make moves.');
     }
-    const game = new Chess();
+    const chess = new Chess();
+    play(player1, player2, chess);
+}
+
+/**
+ * Main chess runtime
+ * @param {GuildMember} player1 White player
+ * @param {GuildMember} player2 Black player
+ * @param {Chess} chess Game board
+ */
+function play(player1, player2, chess) {
     var interface = {
         title: '2 Player Chess Game',
         color: 0xF6D887,
-        description: `${player1}, it's white's turn to move.\n\n\`\`\`${game.ascii()}\`\`\``,
+        description: generateDescription(player1, player2, chess),
         footer: {
-            text: `History: ${game.pgn()}`
+            text: `History: ${chess.pgn()}`
         }
     };
+    while (!chess.game_over())
+}
+
+/**
+ * Generates embed description for game state
+ * @param {GuildMember} player1 White player
+ * @param {GuildMember} player2 Black player
+ * @param {Chess} chess Chess game
+ * @param {Boolean} whiteToMove Is it white's turn to move
+ * @returns {String} Description string for embed
+ */
+function generateDescription(player1, player2, chess, whiteToMove = true) {
+    if (whiteToMove) {
+        return `${player1}, it's white's turn to move.\n\n\`\`\`${chess.ascii()}\`\`\``;
+    } else {
+        return `${player2}, it's black's turn to move.\n\n\`\`\`${chess.ascii()}\`\`\``;
+    }
 }
